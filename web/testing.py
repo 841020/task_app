a92b3fd0094b097cf206b829c86f8a06a3c250c5
Your Name <you@example.com>
2021-10-16 00:13:07 +0800

    update test_index & test_post_task

--- web/testing.py
+++ web/testing.py
@@ -15,9 +15,9 @@ class OrmTest(unittest.TestCase):
     def test_index(self):
         response = self.test_client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get_data(), b"Hello, World!")
+        self.assertEqual(response.get_data().decode(), "Hello, World!")
 
     def test_post_task(self):
         response = self.test_client.post('/task', json={"name": "買晚餐"}, follow_redirects=True)
-        self.assertEqual(response.get_data(), b'{"result":{"id":1,"name":"\\u8cb7\\u665a\\u9910","status":false}}\n')
+        self.assertEqual(response.get_data().decode(), '{"result":{"id":1,"name":"買晚餐","status":false}}\n')
         self.assertEqual(response.status_code, 201)

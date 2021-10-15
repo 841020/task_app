42b64f33827a3bcb6774cd3a64b6dbc67c1e5944
Your Name <you@example.com>
2021-10-16 00:02:29 +0800

    add post task test case

--- web/testing.py
+++ web/testing.py
@@ -16,3 +16,8 @@ class OrmTest(unittest.TestCase):
         response = self.test_client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get_data(), b"Hello, World!")
+
+    def test_post_task(self):
+        response = self.test_client.post('/task', json={"name": "買晚餐"}, follow_redirects=True)
+        self.assertEqual(response.get_data(), b'{"result":{"id":1,"name":"\\u8cb7\\u665a\\u9910","status":false}}\n')
+        self.assertEqual(response.status_code, 201)
